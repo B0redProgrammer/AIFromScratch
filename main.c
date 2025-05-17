306a68d967a62bc887de9747ca472d4ca508c568
B0redProgrammer <incandelha@gmail.com>
2025-05-17 17:26:09 +0200

    implemented matrix vector product

--- main.c
+++ main.c
@@ -1,3 +1,29 @@
+#include "math.c"
+
 int main() {
+  struct Matrix m;
+  
+  m.n = 2;
+  m.m = 3;
+  m.vals = malloc(sizeof(float*)*m.m);
+  for(int i = 0; i<m.m; i++) {
+    m.vals[i] = malloc(sizeof(float)*m.n);
+  }
+
+  m.vals[0][0] = 4; m.vals[0][1] = 2;
+  m.vals[1][0] = 1; m.vals[1][1] = 7;
+  m.vals[2][0] = 5; m.vals[2][1] = 3;
+
+  struct Vector v;
+  v.numVals = 2;
+  v.vals = malloc(sizeof(float)*v.numVals);
+  v.vals[0] = 6;
+  v.vals[1] = 3;
+
+  struct Vector res; 
+  if(matVecMult(&m, &v, &res)) {
+    printVec(&res);
+  }
+
   return 0;
 }
--- math.c
+++ math.c
@@ -1,17 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+
 typedef struct Vector {
   float * vals;
   int numVals;
 };
 
-void vectorMult(struct Vector * vec1, struct Vector * vec2) {
-  if(vec1->numVals != vec2->numVals) {
-    return;
+typedef struct Matrix {
+  float ** vals;
+  int n;
+  int m;
+};
+
+int matVecMult(struct Matrix * m, struct Vector * vec, struct Vector * ret) {
+  if(m->n != vec->numVals) {
+    return 0;
   }
-  for(int i = 0; i<vec1->numVals; i++) {
+
+  ret->numVals = m->m;
+  if((ret->vals = malloc(sizeof(float)*ret->numVals)) == NULL) {
+    return 0;
+  }
+
+  for(int i = 0; i<m->m; i++) {
     float sum = 0.0;
-    for(int j = 0; j<vec2->numVals; j++) {
-      sum += vec1[i]*vec2[j];
+    for(int j = 0; j<m->n; j++) {
+       sum += vec->vals[j] * m->vals[i][j];
     }
-    vec1[i] = sum;
+    ret->vals[i] = sum;
+  }
+  return 1;
+}
+
+void printVec(struct Vector * vec) {
+  printf("Printing Vector: \n");
+  for(int i = 0; i<vec->numVals; i++) {
+    printf("%f,\n", vec->vals[i]);
   }
 }
